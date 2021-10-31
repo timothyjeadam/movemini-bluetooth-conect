b388716ace9f7880a45ee701f983f92093f7bcb9
timothyjeadam <92826294+timothyjeadam@users.noreply.github.com>
2021-10-31 12:24:17 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,20 @@
 bluetooth.onBluetoothConnected(function () {
     basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . # . .
+        . . . . .
+        . # . # .
+        . . . . .
         # . . . #
         . # # # .
         `)
-    basic.showString("MY NAME IS ZEZOT")
 })
 bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.No)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
